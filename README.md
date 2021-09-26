24dd24d67e7b97afb773ac64c5ae382571945ee1
Prashanth <imprash4u@gmail.com>
2021-09-27 01:13:12 +0530

    Added: Welcome Message

---
+++ README.md
@@ -0,0 +1,1 @@
+Address_Books_System
---
+++ src/com/bridgelabz/adressbookssystem/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.adressbookssystem;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome to AddressBooks System");
+    }
+}
